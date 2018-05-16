84986b9346875119b0f65d7cc4fa0cf993aa4692
BERREZOUG <berrezoug@macbook-air-de-berrezoug.home>
2018-05-16 07:45:02 +0200

    Bug Fix

--- UIRestApi/src/services/images.service.ts
+++ UIRestApi/src/services/images.service.ts
@@ -10,7 +10,7 @@ export class ImagesService {
   
     getImages(keyWord:String, size:number, currentPage:number){
       return this.http.get("https://pixabay.com/api/?key=8915602-e8662d005c34937276462ac90&q=" 
-      + keyWord + " &per_page=" + size + "&page=" + currentPage).map(resp=>resp.json());
+      + keyWord + "&per_page=" + size + "&page=" + currentPage).map(resp=>resp.json());
     }
   
 } 
